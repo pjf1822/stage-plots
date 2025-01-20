c9d55bd6f41034e67f55228004a9143ace9102d9
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-01-20 12:54:02 -0500

    ok going to add a bunch of bullshit

--- components/Draggable.tsx
+++ components/Draggable.tsx
@@ -3,9 +3,10 @@ import { useDraggable } from "@dnd-kit/core"; // Make sure this import is correc
 import { CSS } from "@dnd-kit/utilities"; // Make sure this import is correct
 
 const Draggable = (props) => {
-  const { attributes, listeners, setNodeRef, transform } = useDraggable({
-    id: props.id,
-  });
+  const { attributes, listeners, setNodeRef, transform, over, isDragging } =
+    useDraggable({
+      id: props.id,
+    });
 
   const style = {
     // Outputs `translate3d(x, y, 0)`
--- components/Droppable.tsx
+++ components/Droppable.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDroppable } from "@dnd-kit/core";
 
 export function Droppable(props) {
+  //   console.log(props);
   const { isOver, setNodeRef } = useDroppable({
     id: props.id,
   });
