8cc36d4daaf9f528b67cb5984a1c2c466b2eeb74
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-01-23 21:14:22 -0500

    bonk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Provider from "@/utils/Providers";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         <Provider>
           <Navbar />
           {children}
+          <SpeedInsights />
         </Provider>
       </body>
     </html>
