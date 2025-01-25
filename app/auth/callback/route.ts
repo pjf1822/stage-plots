f5c95e0fa8879be4e56950869f7422a297b05a45
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-01-25 16:36:06 -0500

    help

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -12,10 +12,10 @@ export async function GET(request: Request) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original dforigin before load balancer
+      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
-      console.log(origin, searchParams, "hey things", forwardedHost);
       if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
         return NextResponse.redirect(`https://${forwardedHost}${next}`);
@@ -25,5 +25,6 @@ export async function GET(request: Request) {
     }
   }
 
+  // return the user to an error page with instructions
   return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
