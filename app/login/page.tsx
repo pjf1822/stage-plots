2e7c05edc03628d389e1e61f9a18781fa55b6d6c
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-03 15:38:41 -0500

    go

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
   return (
     <div className="flex flex-col justify-around items-center min-h-screen bg-black p-6">
       <Image
-        src="/newLogo.png"
+        src="/newlogo.png"
         alt="Logo"
         width={200}
         height={200}
@@ -33,7 +33,7 @@ export default function LoginPage() {
       <Card className="w-full max-w-xs sm:max-w-sm p-4 sm:p-6 shadow-xl rounded-lg bg-themeOne">
         <Button
           onClick={handleGoogleSignIn}
-          className="w-full bg-themeFive text-themeOne hover:bg-themeOne hover:text-themeFive transition-colors py-2 sm:py-3 px-4 sm:px-6 rounded-lg font-semibold text-lg sm:text-xl font-urbanist flex justify-center items-center leading-[1.2] text-center"
+          className="z-10 px-6 py-3 rounded-full bg-black text-white border border-white hover:bg-white hover:text-black transition-all duration-300 ease-in-out transform hover:scale-105 font-urbanist"
         >
           Sign in with Google
         </Button>
