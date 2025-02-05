cd3383d59a8947d444828b08e2de0b8c2ae00ec6
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-05 11:30:13 -0500

    nice

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,14 +4,34 @@ import Link from "next/link";
 import Image from "next/image";
 import { createClient } from "@/utils/supabase/client";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const supabase = createClient();
   const pathname = usePathname();
+  const [userProfilePic, setUserProfilePic] = useState<string | null>(null);
 
   if (pathname === "/" || pathname === "/privacy") {
     return null;
   }
+  useEffect(() => {
+    const getSession = async () => {
+      try {
+        const { data, error } = await supabase.auth.getSession();
+        if (error) {
+          console.error("Error fetching session:", error);
+          return;
+        }
+        const avatarUrl = data?.session?.user?.user_metadata?.avatar_url || "";
+        console.log("Avatar URL:", avatarUrl);
+        setUserProfilePic(avatarUrl);
+      } catch (err) {
+        console.error("Unexpected error fetching session:", err);
+      }
+    };
+
+    getSession();
+  }, []);
 
   const handleLogout = async () => {
     try {
@@ -38,13 +58,26 @@ const Navbar = () => {
         />
       </Link>
 
-      <Button
-        onClick={handleLogout}
-        variant={"outline"}
-        className="font-urbanist bg-black mr-8 text-lg px-6 py-6 rounded-lg text-white shadow-xl transform transition-all hover:scale-105"
-      >
-        Logout
-      </Button>
+      <div className="flex flex-row">
+        {userProfilePic ? (
+          <Image
+            src={userProfilePic}
+            alt="User Avatar"
+            width={50}
+            height={50}
+            className="rounded-full border border-white mr-8"
+          />
+        ) : (
+          <div className="w-12 h-12 bg-gray-500 rounded-full"></div>
+        )}
+        <Button
+          onClick={handleLogout}
+          variant={"outline"}
+          className="font-urbanist bg-black mr-8 text-lg px-6 py-6 rounded-lg text-white shadow-xl transform transition-all hover:scale-105"
+        >
+          Logout
+        </Button>
+      </div>
     </div>
   );
 };
