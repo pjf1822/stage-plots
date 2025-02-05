42bf8348bd3000e7fe44b8f72c8ed3d9d7f3279e
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-04 20:56:21 -0500

    glorisou

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -20,24 +20,12 @@ export async function GET(request: Request) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
-        const { error: shitError } = await supabase
-          .from("fuckinlogs")
-          .insert({ log_data: `1   ${origin}` });
-
         // we can be sure that there is n load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        const { error: shitError } = await supabase
-          .from("fuckinlogs")
-          .insert({ log_data: `2   ${forwardedHost} and${next}` });
-
-        return NextResponse.redirect("https://thestageplotter.com/dashboard");
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
       } else {
-        const { error: shitError } = await supabase
-          .from("fuckinlogs")
-          .insert({ log_data: `3 ${origin} and${next}` });
-
-        return NextResponse.redirect("https://thestageplotter.com/dashboard");
+        return NextResponse.redirect(`${origin}${next}`);
       }
     }
   }
