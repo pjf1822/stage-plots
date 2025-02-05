8022a9c922491d42165ab5a5e2c057550bb1fb19
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-04 20:33:15 -0500

    nice

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,27 +6,36 @@ export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
 
   const code = searchParams.get("code");
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/dashboard";
 
-  console.log(next, "next");
   if (code) {
     const supabase = await createClient();
+
+    const { error: shitError } = await supabase
+      .from("fuckinlogs")
+      .insert({ log_data: `0   ${code} and fucking ${next}` });
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-    console.log(error);
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
       if (isLocalEnv) {
-        console.log("1", origin, next);
+        const { error: shitError } = await supabase
+          .from("fuckinlogs")
+          .insert({ log_data: `1   ${origin}` });
+
         // we can be sure that there is n load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        console.log("2", forwardedHost, next);
+        const { error: shitError } = await supabase
+          .from("fuckinlogs")
+          .insert({ log_data: `2   ${forwardedHost} and${next}` });
 
         return NextResponse.redirect(`https://thestageplotter.com/dashboard`);
       } else {
-        console.log("3", origin, next);
+        const { error: shitError } = await supabase
+          .from("fuckinlogs")
+          .insert({ log_data: `3 ${origin} and${next}` });
 
         return NextResponse.redirect(`${origin}${next}`);
       }
