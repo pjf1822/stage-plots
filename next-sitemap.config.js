31ce9bdb9af56f9d6726fc87fccc4f17ece8ba1c
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-09 18:23:41 -0500

    ok

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: "https://www.thestageplotter.com", // Replace with your site URL
+  generateRobotsTxt: true, // Optionally generate robots.txt
+  sitemapSize: 7000, // Optional, limit the number of URLs per sitemap
+  outDir: "./public", // Output directory for the sitemap and robots.txt files
+  changefreq: "daily", // How often the pages are likely to change
+  priority: 0.7, // Default priority for the pages (0.0 to 1.0)
+};
