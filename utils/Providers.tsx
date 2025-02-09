8f8c5af7a9037166ae2812cbe23e791963f7d913
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2025-02-09 15:46:56 -0500

    ok sure

--- utils/Providers.tsx
+++ utils/Providers.tsx
@@ -10,7 +10,7 @@ export default function Provider({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
